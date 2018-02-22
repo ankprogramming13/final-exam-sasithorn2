06b435bef62ab510a2f89fff890e7a2749e6f006
Unknown <toeyhom64@gmail.com>
2018-02-22 12:18:24 +0700

    เกรด4

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+using namespace std;
 int main() {
+    int a,b,cmd,reusult_1,reusult_2,reusult_3,reusult_4,reusult_5;
+    cout << "welcome to CLI Calculator V1.00" << endl;
+    cout << "Number 1 : ";
+    cin >> a;
+    cout << "Number 2 : ";
+    cin >> b;
+    do{
+        cout << "menu\n1.+\n2.-\n3.*\n4./\n5.quit" << endl;
+        cin >> cmd;
+        if(cmd==1){
+            reusult_1=a+b;
+            cout << "result = "<<reusult_1 << endl;
+        }else if(cmd==2) {
+            reusult_2=a-b;
+            cout << "result = " << reusult_2 << endl;
+         }else if(cmd==3){
+            reusult_3=a*b;
+            cout << "result = " << reusult_3 << endl;
+          }else if(cmd==4){
+            reusult_4=a/b;
+            cout << "result = " << reusult_4 << endl;
 
+
+
+            }break;
+        }while(1);
         return 0;
 }
